60f82cab7c381c5f467c5309ab0260b645295406
Fython <fang.jia.hui123@gmail.com>
2017-12-21 10:12:16 +0800

    finish sample crawler

--- Crawler/web_scraping.py
+++ Crawler/web_scraping.py
@@ -6,6 +6,7 @@ from urllib.error import URLError
 from urllib.parse import urljoin, urlparse
 from functools import partial
 from datetime import datetime
+from collections import deque
 import re
 import time
 
@@ -22,27 +23,21 @@ def download(url, user_agent, num_retries=2):
             if hasattr(e, 'code') and 500 <= getattr(e, 'code') < 600:
                 return download(url, user_agent, num_retries - 1)
         html = None
+    except BaseException as e:
+        print(e)
     return html
 
 
-def link_crawler(seed_url, link_regex):
-    link_queue = [seed_url]
-    seen = set(link_queue)
-
-    while link_queue:
-        link = link_queue.pop()
-        for l in get_links(link):
-            if re.search(link_regex, l):
-                if l not in seen:
-                    seen.add(l)
-                    link_queue.append(l)
-
-
-def get_links(link):
-    html = download(link, 'Mozilla')
+def get_links(html, org_link):
     regex = re.compile(r'<a[^>]*?href=[\"\'](.*?)[\"\'].*?>', re.IGNORECASE)
-    my_urljoin = partial(urljoin, link)
-    return map(my_urljoin, re.findall(regex, html))
+    my_urljoin = partial(urljoin, org_link)
+    links = map(my_urljoin, re.findall(regex, html))
+    links = [link.rsplit('?', maxsplit=1)[0] for link in links]
+
+    def same_domain(url_1, url_2):
+        return urlparse(url_1).netloc == urlparse(url_2).netloc
+
+    return [l for l in links if same_domain(l, org_link)]
 
 
 class Throttle(object):
@@ -60,5 +55,53 @@ class Throttle(object):
                 time.sleep(self.delay_time)
         self.domain[netloc] = datetime.now()
 
+
+def main(url, link_regex, delay=-1, retries=2, max_depth=-1, max_download=-1, user_agent='Mozilla'):
+    """
+    :param url: url
+    :param link_regex: filter link
+    :param delay: less than zero unlimted
+    :param retries: how many retry a url
+    :param max_depth: rember url depth if max_depth is less than zero unlimted
+    :param max_download: download pages if less than zero behalf unlimted
+    :param user_agent: User-Agent
+    """
+    crawl_queue = deque([url])
+    # check weather crawled and depth
+    seen = {url: 0}
+    slow = Throttle(delay)
+    downloaded = 0
+
+    while crawl_queue:
+        link = crawl_queue.pop()
+        # current page depth
+        depth = seen[link]
+        if depth > max_depth:
+            continue
+
+        slow.wait(link)
+        html = download(link, user_agent, retries)
+        # get this page all links
+        links = get_links(html, link)
+
+        for l in links:
+            if l not in seen and re.search(link_regex, l):
+                # add crawl queue
+                crawl_queue.append(l)
+                # set seen
+                seen[l] = depth + 1
+
+        # increase download number
+        downloaded += 1
+        if downloaded == max_download:
+            break
+
+    # print(seen)
+    # print(downloaded)
+
+
+if __name__ == '__main__':
+    main('http://example.webscraping.com/',
+         '/places/default/(view|index)', max_depth=3, max_download=50, delay=2)
+
 # https://bitbucket.org/wswp/code/src/9e6b82b47087c2ada0e9fdf4f5e037e151975f0f/chapter01/link_crawler3.py?at=default&fileviewer=file-view-default
-link_crawler('http://example.webscraping.com/', '/places/default/(view|index)')
