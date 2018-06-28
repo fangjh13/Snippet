f734aa7c8ff0a9ac054807887160b686093638e0
Fython <fang.jia.hui123@gmail.com>
2018-06-29 00:00:52 +0800

    use WebKit and search and return result

--- Crawler/webkit_pyqt.py
+++ Crawler/webkit_pyqt.py
@@ -3,15 +3,23 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebKit import *
 from PyQt5.QtWebKitWidgets import *
-import lxml.html
 
-url = 'https://www.baidu.com'
+url = 'http://example.webscraping.com/places/default/search'
 app = QApplication([])
 webview = QWebView()
 loop = QEventLoop()
 webview.loadFinished.connect(loop.quit)
 webview.load(QUrl(url))
 loop.exec_()
-html = webview.page().mainFrame().toHtml()
-tree = lxml.html.fromstring(html)
-print(tree.cssselect('#su')[0].value)
+webview.show()
+frame = webview.page().mainFrame()
+frame.findFirstElement('#search_term').setAttribute('value', '.')
+frame.findFirstElement('#page_size option:checked').setPlainText('1000')
+frame.findFirstElement('#search').evaluateJavaScript('this.click()')
+# app.exec_()
+elements = None
+while not elements:
+    app.processEvents()
+    elements = frame.findAllElements('#results a')
+countries = [e.toPlainText().strip() for e in elements]
+print(countries)
