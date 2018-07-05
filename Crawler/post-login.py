743a7a12e0428c2fa3537b95a390edcfd6418df7
Fython <fang.jia.hui123@gmail.com>
2018-07-05 11:19:29 +0800

    use requests Session post form data

--- Crawler/post-login.py
+++ Crawler/post-login.py
@@ -2,16 +2,17 @@ from urllib.request import urlopen, Request, build_opener, HTTPCookieProcessor
 from urllib.parse import urlencode
 from http.cookiejar import CookieJar
 import lxml.html
+import requests
 
 
 url = 'http://example.webscraping.com/places/default/user/login'
 LOGIN_EMAIL = 'example1234@qq.com'
 PASSWORD = '123456'
 
-cj = CookieJar()
-opener = build_opener(HTTPCookieProcessor(cj))
+# cj = CookieJar()
+# opener = build_opener(HTTPCookieProcessor(cj))
 
-html = opener.open(url).read().decode('utf-8')
+# html = opener.open(url).read().decode('utf-8')
 
 
 def parse_form(html):
@@ -23,11 +24,18 @@ def parse_form(html):
     return data
 
 
+# data = parse_form(html)
+# data['email'] = LOGIN_EMAIL
+# data['password'] = PASSWORD
+
+
+# r = Request(url, data=urlencode(data).encode('utf-8'))
+# response = opener.open(r)
+# print(response.geturl())
+r =  requests.Session()
+html = r.get(url).text
 data = parse_form(html)
 data['email'] = LOGIN_EMAIL
 data['password'] = PASSWORD
-
-
-r = Request(url, data=urlencode(data).encode('utf-8'))
-response = opener.open(r)
-print(response.geturl())
+result = r.post(url, data=data)
+print(result.url)
